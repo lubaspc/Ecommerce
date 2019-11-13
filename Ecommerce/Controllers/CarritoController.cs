4d275821ca033bc22e7ae3d3772c3594a7e6d76a
lubaspc <lubinpc@gmail.com>
2019-11-13 15:30:15 -0600

    Agregar carrito

---
+++ Ecommerce/Controllers/CarritoController.cs
@@ -0,0 +1,64 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class CarritoController : Controller
+    {
+        // GET: Carrito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Comprar(string id)
+        {
+            Productos productos = new Productos();
+            if (Session["carro"] == null)
+            {
+                List<Carrito> carro = new List<Carrito>();
+                carro.Add(new Carrito{ Productos = productos, Cantidad = 1 });
+                Session["carro"] = carro;
+            }
+            else
+            {
+                List<Carrito> carro = (List<Carrito>)Session["carro"];
+                int index = isExist(id);
+                if (index != -1)
+                {
+                    carro[index].Cantidad++;
+                }
+                else
+                {
+                    carro.Add(new Carrito { Productos = productos, Cantidad = 1 });
+                }
+                Session["carro"] = carro;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Eliminar(string id)
+        {
+            List<Carrito> carro = (List<Carrito>)Session["carro"];
+            int index = isExist(id);
+            carro.RemoveAt(index);
+            Session["carro"] = carro;
+            return RedirectToAction("Index");
+        }
+
+        private int isExist(String id)
+        {
+            List<Carrito> carro = (List<Carrito>)Session["carro"];
+            for (int i = 0; i < carro.Count; i++)
+            {
+                if (carro[i].Productos.Id.Equals(id))
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
--- Ecommerce/Migrations/Configuration.cs
+++ Ecommerce/Migrations/Configuration.cs
@@ -14,10 +14,10 @@
 
         protected override void Seed(Ecommerce.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.Catalogos.AddOrUpdate(new Models.Catalogos
+            {
+                name = "Pan"
+            });
         }
 
     }
